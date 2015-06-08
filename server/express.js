15b5d2ebce5b7975ffab3931f5c2859081d16d40
Brian Mitchell <bman4789@gmail.com>
2015-06-07 23:04:00 -0500

    Loads analytics if in production environment

--- server/express.js
+++ server/express.js
@@ -21,6 +21,7 @@ module.exports = function(app, config) {
   var env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
+  app.locals.ENV_PRODUCTION = env == 'production';
 
   app.use(favicon(config.root + '/public/img/favicon.ico'));
   app.use(logger('dev'));
